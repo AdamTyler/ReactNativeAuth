89c52a5df0131ebea8bdb63458a507faf063570e
Adam Tyler <lazy9669@gmail.com>
2017-05-27 10:35:59 -0500

    Adding spinner and auth function

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import { TextInput } from 'react-native'
-import { Button, Card, CardSection, Input } from './common'
+import { Text } from 'react-native'
+import firebase from 'firebase'
+import { Button, Card, CardSection, Input, Spinner } from './common'
 
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: '',
+    submitting: false
   }
 
   render() {
+    const { errorStyle } = styles
     return (
       <Card>
         <CardSection>
@@ -27,13 +31,52 @@ class LoginForm extends Component {
             onChange={password => this.setState({ password })}/>
         </CardSection>
 
+        <Text style={errorStyle}>{this.state.error}</Text>
+
         <CardSection>
-          <Button>
-            Log In
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     )
+  }
+
+  loginUser() {
+    const { email, password } = this.state
+    this.setState({error: '', submitting: true})
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch((ex) => {
+        this.setState({error: 'Authentication Error', submitting: false})
+      })
+      .then((res) => {
+        console.log('signup', res)
+        this.setState({submitting: false})
+      })
+    })
+    .then(res => {
+      console.log('login', res)
+      this.setState({submitting: false})
+    })
+  }
+
+  renderButton() {
+    if(this.state.submitting) {
+      return <Spinner size={'small'} />
+    }
+    return (
+      <Button onPress={this.loginUser.bind(this)}>
+        Log In
+      </Button>
+    )
+  }
+}
+
+const styles = {
+  errorStyle: {
+    alignSelf: 'center',
+    color: 'red',
+    fontSize: 20
   }
 }
 
